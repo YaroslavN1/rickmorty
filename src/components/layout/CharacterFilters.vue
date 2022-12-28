a411192ea5e319cf3ad4015f07368cbb29ccc1e9
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2022-12-28 16:50:58 +0100

    Added ability to store and retrieve filters from Session Storage

--- src/components/layout/CharacterFilters.vue
+++ src/components/layout/CharacterFilters.vue
@@ -2,11 +2,11 @@
   <div class="filters-wrapper grid grid-cols-1 gap-1">
     <input
       id="characterName"
-      v-model="characterName"
+      v-model="selectedFilters.name"
       placeholder="Search by name..."
       type="text"
       class="justify-self-center w-2/5 h-7 mt-2 rounded border text-center focus:border-green-300 focus:ring-5 focus:ring-green-200 outline-none transition-colors duration-200 ease-in-out"
-      @input="storeCharacters.searchByName(characterName)"
+      @input="storeCharacters.searchByName(selectedFilters.name)"
     >
     <label for="dropdown">
       <span
@@ -16,10 +16,10 @@
       </span>
       <select
         id="filters"
-        v-model="selectedFilter.status"
+        v-model="selectedFilters.status"
         class="capitalize outline-0"
         name="filters"
-        @change="setFilter('status', selectedFilter.status)"
+        @change="setFilter('status', selectedFilters.status)"
       >
         <option selected value="all">
           - all
@@ -43,10 +43,10 @@
       </span>
       <select
         id="filters"
-        v-model="selectedFilter.species"
+        v-model="selectedFilters.species"
         class="capitalize outline-0"
         name="filters"
-        @change="setFilter('species', selectedFilter.species)"
+        @change="setFilter('species', selectedFilters.species)"
       >
         <option selected value="all">
           - all
@@ -70,10 +70,10 @@
       </span>
       <select
         id="filters"
-        v-model="selectedFilter.type"
+        v-model="selectedFilters.type"
         class="capitalize outline-0"
         name="filters"
-        @change="setFilter('type', selectedFilter.type)"
+        @change="setFilter('type', selectedFilters.type)"
       >
         <option selected value="all">
           - all
@@ -97,10 +97,10 @@
       </span>
       <select
         id="filters"
-        v-model="selectedFilter.gender"
+        v-model="selectedFilters.gender"
         class="capitalize outline-0"
         name="filters"
-        @change="setFilter('gender', selectedFilter.gender)"
+        @change="setFilter('gender', selectedFilters.gender)"
       >
         <option selected value="all">
           - all
@@ -157,18 +157,21 @@
   characters id range
 */
 
+  // const idRange = ref([1, 20])
   const idRange = () => {
     let range = []
-    let arr = [...storeCharacters.characterItems]
-    range.push(storeCharacters.characterItems[0]?.id)
-    range.push(arr.reverse()[0]?.id)
-    return range
+    if(storeCharacters.requestFilters.page !== storeCharacters.lastPage) {
+      range.push(1 + (storeCharacters.requestFilters.page - 1) * storeCharacters.characterItems.length)
+      range.push(storeCharacters.requestFilters.page * storeCharacters.characterItems.length)
+      return range
+    } else {
+      range.push(storeCharacters.charactersCount - (storeCharacters.characterItems.length - 1))
+      range.push(storeCharacters.charactersCount)
+      return range
+    }
   }
-
-  console.log(idRange())
-
 /* 
-  filters
+  get filters
 */
 
   const filterCategories = storeCharacters.filterCategories
@@ -176,26 +179,39 @@
   const [{subFilters: statusFilters}, {subFilters: speciesFilters}, {subFilters: typeFilters}, {subFilters: genderFilters}] = filterCategories
 
 /* 
-  selected filters
+  selected filters default
 */
 
-  const selectedFilter = ref({
+  const selectedFilters = ref({
+    name: '',
     status: 'all',
     species: 'all',
     type: 'all',
     gender: 'all'
   })
+
+/* 
+  update selected filters from session storage
+*/
+
+  Object.keys(selectedFilters.value).forEach(el => {
+        if(storeCharacters.requestFilters[el]) {
+          selectedFilters.value[el] = storeCharacters.requestFilters[el]
+        }
+      })
   
+/* 
+  reset filters
+*/
   const resetFilters = () => {
-    selectedFilter.value.status = 'all'
-    selectedFilter.value.species = 'all'
-    selectedFilter.value.type = 'all'
-    selectedFilter.value.gender = 'all'
-    characterName.value = ''
+    selectedFilters.value.status = 'all'
+    selectedFilters.value.species = 'all'
+    selectedFilters.value.type = 'all'
+    selectedFilters.value.gender = 'all'
+    selectedFilters.value.name = ''
     storeCharacters.resetStoreFilters()
   }
 
-  const characterName = ref('')
 
 /* 
   set request filter in store
