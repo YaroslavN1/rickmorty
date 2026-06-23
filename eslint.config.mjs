09513e8c582d83a4e1dff4e772220412f1a18a21
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2026-06-23 20:23:19 +0200

    chore(deps): configured prettier along with tailwindcss plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default [
-  ...pluginVue.configs['flat/recommended'],
-]
+export default [...pluginVue.configs['flat/recommended'], eslintConfigPrettier]
