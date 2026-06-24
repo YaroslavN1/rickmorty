ed4984a92758a4bdc3f33c5d375ab699286bba70
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2026-06-24 17:40:36 +0200

    refactor: extracted repeated <button> code into NavigationButton component

--- src/components/layout/NavigationButtons.vue
+++ src/components/layout/NavigationButtons.vue
@@ -1,56 +1,57 @@
 <template>
   <div class="flex h-8 justify-center">
-    <button
-      :disabled="storeCharacters.requestFilters.page === 1"
-      class="rounded border bg-white px-4 drop-shadow-sm transition-colors duration-100 ease-in-out hover:border-green-300 focus:outline-none disabled:pointer-events-none disabled:text-slate-200"
+    <NavigationButton
+      :disabled="isFirstPage"
       @click="storeCharacters.goToPage(1)"
     >
       First
-    </button>
+    </NavigationButton>
 
-    <button
-      :disabled="storeCharacters.requestFilters.page <= 1"
-      class="ml-5 rounded border bg-white px-4 drop-shadow-sm hover:border-green-300 focus:outline-none disabled:pointer-events-none disabled:text-slate-200"
+    <NavigationButton
+      class="ml-5"
+      :disabled="isFirstPage"
       @click="storeCharacters.movePage(-1)"
     >
       &lt;
-    </button>
+    </NavigationButton>
 
     <input
       id="pageNumber"
       :value="storeCharacters.requestFilters.page"
       type="number"
-      class="mx-1 w-14 rounded border border-gray-100 bg-white text-center text-gray-700 outline-none drop-shadow-sm transition-colors duration-200 ease-in-out focus:border-green-300"
+      class="mx-1 w-14 rounded border border-gray-100 text-center outline-none drop-shadow-sm focus:border-green-300"
       @focusout="onPageNumberInput"
       @keyup.enter="onPageNumberInput"
     />
 
-    <button
-      :disabled="
-        storeCharacters.requestFilters.page >= storeCharacters.lastPage
-      "
-      class="rounded border bg-white px-4 drop-shadow-sm hover:border hover:border-green-300 focus:outline-none disabled:pointer-events-none disabled:text-slate-200"
+    <NavigationButton
+      :disabled="isLastPage"
       @click="storeCharacters.movePage(1)"
     >
       &gt;
-    </button>
+    </NavigationButton>
 
-    <button
-      :disabled="
-        storeCharacters.requestFilters.page >= storeCharacters.lastPage
-      "
-      class="ml-5 rounded border bg-white px-4 drop-shadow-sm hover:border-green-300 focus:outline-none disabled:pointer-events-none disabled:text-slate-200"
+    <NavigationButton
+      class="ml-5"
+      :disabled="isLastPage"
       @click="storeCharacters.goToPage(storeCharacters.lastPage)"
     >
       Last
-    </button>
+    </NavigationButton>
   </div>
 </template>
 
 <script setup>
+import NavigationButton from './NavigationButton.vue'
 import { useStoreCharacters } from '@/stores/storeCharacters'
+import { computed } from 'vue'
 
 const storeCharacters = useStoreCharacters()
+
+const isFirstPage = computed(() => storeCharacters.requestFilters.page === 1)
+const isLastPage = computed(
+  () => storeCharacters.requestFilters.page === storeCharacters.lastPage,
+)
 
 const onPageNumberInput = ({ target: { value } }) => {
   storeCharacters.requestFilters.page = Math.max(
