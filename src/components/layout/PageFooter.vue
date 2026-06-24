d776c5f28b3722864e590ca8cbd69f8cd3a97262
YaroslavNykyforuk <105391525+YaroslavN1@users.noreply.github.com>
2026-06-24 15:18:09 +0200

    fix: header/footer asset urls; clean-up tags and classes (#9)
    * fix: fixed asset url for <source> tags
    
    * refactor: removed redundant tags and classes

--- src/components/layout/PageFooter.vue
+++ src/components/layout/PageFooter.vue
@@ -1,39 +1,29 @@
 <template>
-  <footer class="body-font text-gray-600">
+  <footer>
     <div
-      class="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row"
+      class="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row sm:justify-between"
     >
       <picture class="w-40">
-        <source
-          class="object-cover"
-          type="image/webp"
-          srcset="@/images/rickandmorty_logo_black.webp"
-        />
-        <source
-          class="object-cover"
-          type="image/png"
-          srcset="@/images/rickandmorty_logo_black.png"
-        />
-        <img
-          src="@/images/rickandmorty_logo_black.png"
-          class="object-cover"
-          alt="avatar"
-        />
+        <source type="image/webp" :srcset="logoWebpBlack" />
+        <img :src="logoPngBlack" class="object-cover" alt="avatar" />
       </picture>
-      <span
-        class="mt-4 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start"
-      >
+      <span class="mt-4 inline-flex justify-center sm:mt-0 sm:justify-start">
         <a href="https://github.com/YaroslavN1/rickmorty" target="_blank">
-          <i class="fa-brands fa-github fa-lg text-gray-800" />
+          <i class="fa-brands fa-github fa-lg" />
         </a>
         <a
           href="https://www.linkedin.com/in/yaroslav-nykyforuk-12609a167"
           class="ml-3"
           target="_blank"
         >
-          <i class="fa-brands fa-linkedin fa-lg text-gray-800" />
+          <i class="fa-brands fa-linkedin fa-lg" />
         </a>
       </span>
     </div>
   </footer>
 </template>
+
+<script setup>
+import logoWebpBlack from '@/images/rickandmorty_logo_black.webp'
+import logoPngBlack from '@/images/rickandmorty_logo_black.png'
+</script>
--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -1,23 +1,13 @@
 <template>
-  <header class="body-font bg-green-400 bg-opacity-95 text-white">
-    <div class="container mx-auto flex flex-col items-center p-3">
-      <picture class="w-64">
-        <source
-          class="object-cover"
-          type="image/webp"
-          srcset="@/images/rickandmorty_logo.webp"
-        />
-        <source
-          class="object-cover"
-          type="image/png"
-          srcset="@/images/rickandmorty_logo.png"
-        />
-        <img
-          src="@/images/rickandmorty_logo.png"
-          class="object-cover"
-          alt="avatar"
-        />
-      </picture>
-    </div>
+  <header class="flex flex-col items-center bg-green-400 py-3">
+    <picture class="w-64">
+      <source type="image/webp" :srcset="logoWebp" />
+      <img :src="logoPng" class="object-cover" alt="avatar" />
+    </picture>
   </header>
 </template>
+
+<script setup>
+import logoWebp from '@/images/rickandmorty_logo.webp'
+import logoPng from '@/images/rickandmorty_logo.png'
+</script>
