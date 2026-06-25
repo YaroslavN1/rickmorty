9df0d663bed53f301104ca5dfc6de11628de2bb5
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2026-06-25 16:38:22 +0200

    refactor: combined repeated code for empty states

--- src/components/characters/CharactersList.vue
+++ src/components/characters/CharactersList.vue
@@ -1,18 +1,11 @@
 <template>
   <section class="flex flex-1 flex-col">
     <div
-      v-if="storeCharacters.charactersLoading"
-      class="flex flex-1 items-center justify-center text-xl text-gray-400"
-    >
-      Loading...
-    </div>
-
-    <div
-      v-else-if="storeCharacters.characterItems.length > 0"
+      v-if="characterItems.length > 0"
       class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 md:gap-6"
     >
       <CharacterItem
-        v-for="character in storeCharacters.characterItems"
+        v-for="character in characterItems"
         :key="character.id"
         :character="character"
       />
@@ -22,7 +15,7 @@
       v-else
       class="flex flex-1 items-center justify-center text-xl text-gray-400"
     >
-      No characters found...
+      {{ charactersLoading ? 'Loading...' : 'No characters found...' }}
     </div>
   </section>
 </template>
@@ -30,6 +23,7 @@
 <script setup>
 import CharacterItem from '@/components/characters/character/CharacterItem.vue'
 import { useStoreCharacters } from '@/stores/storeCharacters'
+import { storeToRefs } from 'pinia'
 
-const storeCharacters = useStoreCharacters()
+const { charactersLoading, characterItems } = storeToRefs(useStoreCharacters())
 </script>
