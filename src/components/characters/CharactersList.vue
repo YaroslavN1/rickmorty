e33b95c6088de5e05c48d8d41f09aa940d8e480b
YaroslavNykyforuk <105391525+YaroslavN1@users.noreply.github.com>
2026-06-25 16:53:40 +0200

    refactor: clean-up CharactersList component (#13)
    * fix: empty states take full height of vieport
    
    * fix: condition for showing loaded items relied on a wrong variable
    
    * refactor: combined repeated code for empty states

--- src/components/characters/CharactersList.vue
+++ src/components/characters/CharactersList.vue
@@ -1,30 +1,21 @@
 <template>
-  <section class="h-full">
-    <div class="mx-auto">
-      <div
-        v-if="storeCharacters.charactersLoading"
-        class="flex max-w-full items-center justify-center text-2xl text-gray-200"
-      >
-        Characters loading...
-      </div>
+  <section class="flex flex-1 flex-col">
+    <div
+      v-if="characterItems.length > 0"
+      class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 md:gap-6"
+    >
+      <CharacterItem
+        v-for="character in characterItems"
+        :key="character.id"
+        :character="character"
+      />
+    </div>
 
-      <div
-        v-else-if="storeCharacters.charactersCount > 0"
-        class="grid grid-cols-2 justify-items-center gap-4 sm:grid-cols-3 md:grid-cols-4 md:gap-6"
-      >
-        <CharacterItem
-          v-for="character in storeCharacters.characterItems"
-          :key="character.id"
-          :character="character"
-        />
-      </div>
-
-      <div
-        v-else
-        class="flex max-w-full items-center justify-center text-2xl text-gray-200"
-      >
-        No characters found...
-      </div>
+    <div
+      v-else
+      class="flex flex-1 items-center justify-center text-xl text-gray-400"
+    >
+      {{ charactersLoading ? 'Loading...' : 'No characters found...' }}
     </div>
   </section>
 </template>
@@ -32,6 +23,7 @@
 <script setup>
 import CharacterItem from '@/components/characters/character/CharacterItem.vue'
 import { useStoreCharacters } from '@/stores/storeCharacters'
+import { storeToRefs } from 'pinia'
 
-const storeCharacters = useStoreCharacters()
+const { charactersLoading, characterItems } = storeToRefs(useStoreCharacters())
 </script>
