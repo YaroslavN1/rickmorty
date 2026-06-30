29ed82b41ab8058f413a6e1fc7e9f1b06dfc8bd4
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2026-06-30 18:38:06 +0200

    refactor: typescript setup; converted relevant .js to .ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,20 @@
 import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import vueParser from 'vue-eslint-parser'
 
-export default [...pluginVue.configs['flat/recommended'], eslintConfigPrettier]
+export default tseslint.config(
+  { ignores: ['dist/**'] },
+  ...pluginVue.configs['flat/recommended'],
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  eslintConfigPrettier,
+)
